05367f0868d0eeac33062aaecad3776785c8f13a
Molchanov Ihor <=>
2024-03-25 23:07:20 +0200

    removed files

--- cinema/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import movie_list, movie_detail
-
-urlpatterns = [
-    path("movies/", movie_list, name="movies_list"),
-    path("movies/<int:pk>/", movie_detail, name="movies_detail"),
-]
